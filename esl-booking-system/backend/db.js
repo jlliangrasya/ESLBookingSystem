7945affba8bbffb88edf603adc646a63631d0ffc
Jillian Grace D. Burila <jillianburila@gmail.com>
2025-08-28 11:59:29 +0800

    for heroku

--- esl-booking-system/backend/db.js
+++ esl-booking-system/backend/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+      }
+);
 
 module.exports = pool;
